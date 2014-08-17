dca5b74f049ed1955858a431f0d06663b9643b85
matthieucham <matthieu.cham@gmail.com>
2014-08-17 10:54:11 +0200

    share dbconfig structure

---
+++ kamoulcup/includes/dbConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+  Class dbConfig
+  {
+    public $config = array(
+    'db_server'    => 'localhost',
+    'db_name'      => 'kamoulcup',
+    'db_username'  => 'TODO',
+    'db_password'  => 'TODO'
+    );
+  } 
+ ?>
