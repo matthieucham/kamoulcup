43d415fece6bb7bc169501364528bd466164196f
matthieucham <matthieu.cham@gmail.com>
2014-11-07 21:02:32 +0100

    fix
    fi

--- kamoulcup/km/ctrl/mercatoManager.php
+++ kamoulcup/km/ctrl/mercatoManager.php
@@ -3,7 +3,7 @@ include_once("../../includes/db.php");
 
 function getCurrentMercato() {
     global $db;
-    $merkatoQ="select mer_id,date_format(mer_date_ouverture,'%d/%m/%Y %H:%i') as dateOuverture,date_format(mer_date_ouverture,'%d/%m/%Y %H:%i') as dateFermeture from km_mercato where mer_date_ouverture<now() and mer_date_fermeture>now() and mer_processed=0 limit 1";
+    $merkatoQ="select mer_id,date_format(mer_date_ouverture,'%d/%m/%Y %H:%i') as dateOuverture,date_format(mer_date_fermeture,'%d/%m/%Y %H:%i') as dateFermeture from km_mercato where mer_date_ouverture<now() and mer_date_fermeture>now() and mer_processed=0 limit 1";
     $merkato=$db->getArray($merkatoQ);
     if ($merkato != NULL) {
         return $merkato[0];
