3e9365ae85d4e706bcf380b3bce9ef09273b0efe
matthieucham <matthieu.cham@gmail.com>
2014-11-27 21:31:50 +0100

    Fix resolve mercato

--- kamoulcup/km/admin/process/resolveMercato.php
+++ kamoulcup/km/admin/process/resolveMercato.php
@@ -7,11 +7,11 @@ include('../../../process/validateForm.php');
 $mercatoId = $_POST['mercatoId'];
 
 // Requête de sélection des gagnants d'enchères
-$encheresQ = "SELECT o2.off_joueur_id,o2.off_ekyp_id, o2.off_montant, count(o2.off_ekyp_id) as nbMaxEnchere FROM (
+$encheresQ = "SELECT o2.off_joueur_id,o2.off_inscription_id, o2.off_montant, count(o2.off_inscription_id) as nbMaxEnchere FROM (
 select off_joueur_id,max(off_montant) as off_montant from km_offre WHERE off_mercato_id={$mercatoId} group by off_joueur_id
 ) o1 join km_offre o2 on (o2.off_montant=o1.off_montant and o2.off_joueur_id=o1.off_joueur_id) group by o2.off_joueur_id";
 // Requete pour connaitre la journee courante
-$journeeQ = "select id from journee where date < now() order by date desc limit 1";
+$journeeQ = "select id from journee inner join km_championnat_round on cro_journee_id=journee.id inner join km_mercato on cro_championnat_id=mer_championnat_id where cro_status='PROCESSED' and mer_id={$mercatoId} order by journee.date desc limit 1";
 $lastJournee = 0;
 $currentJourneeArray = $db->getArray($journeeQ);
 if (currentJourneeArray != NULL) {
@@ -20,7 +20,7 @@ if (currentJourneeArray != NULL) {
 
 $encheresMax = $db->getArray($encheresQ);
 $redistribution = array();
-$franchisesQ = "select id from ekyp where km=1";
+$franchisesQ = "select ins_id from km_inscription inner join km_championnat on chp_id=ins_championnat_id inner join km_mercato on mer_championnat_id=chp_id where mer_id={$mercatoId}";
 $franchises = $db->getArray($franchisesQ);
 $nbEkyps=sizeof($franchises);
 // init du tableau
@@ -33,7 +33,7 @@ if ($encheresMax != NULL) {
 	foreach ($encheresMax as $value) {
 		if($value['nbMaxEnchere'] == 1) {
 			// On peut maintenant enregistrer l'engagement
-			$redistribution = registerEngagement($value['off_ekyp_id'],$value['off_joueur_id'],$value['off_montant'],$lastJournee,$redistribution,$nbEkyps,$mercatoId);
+			$redistribution = registerEngagement($value['off_inscription_id'],$value['off_joueur_id'],$value['off_montant'],$lastJournee,$redistribution,$nbEkyps,$mercatoId);
 
 		} else {
 			// Les égalités sont conservées pour traitement ultérieur.
@@ -45,13 +45,13 @@ if ($encheresMax != NULL) {
 	for ($i = 0; $i < $nbEq; $i++) {
 		// Recup des tops encheres sur ce joueur
 		$joueurId = $joueursEgalite[$i];
-		$vainqueursPotentielsQ="SELECT off_ekyp_id,off_montant FROM `km_offre` WHERE off_joueur_id={$joueurId} AND off_montant =
-			(SELECT max( off_montant ) FROM km_offre WHERE off_joueur_id={$joueurId} ) ";
+		$vainqueursPotentielsQ="SELECT off_inscription_id,off_montant FROM `km_offre` WHERE off_joueur_id={$joueurId} AND off_montant =
+			(SELECT max( off_montant ) FROM km_offre WHERE off_joueur_id={$joueurId} ) AND off_mercato_id={$mercatoId} ";
 		$vainqueursPotentiels = $db->getArray($vainqueursPotentielsQ);
 		$nbVP = sizeof($vainqueursPotentiels);
 		// Tirage aléatoire
 		$index = rand(0,$nbVP-1);
-		$vainqueur = $vainqueursPotentiels[$index]['off_ekyp_id'];
+		$vainqueur = $vainqueursPotentiels[$index]['off_inscription_id'];
 		$redistribution = registerEngagement($vainqueur,$joueurId,$vainqueursPotentiels[$index]['off_montant'],$lastJournee,$redistribution,$nbEkyps,$mercatoId);
 	}
 }
@@ -66,12 +66,12 @@ foreach($redistribution as $ekyp => $prime) {
 	}
 }
 
-function registerEngagement($ekypId,$joueurId,$montant,$journee,$redistrib, $nbFranchises, $mercatoId){
+function registerEngagement($inscriptionId,$joueurId,$montant,$journee,$redistrib, $nbFranchises, $mercatoId){
 	global $db;
 	// marquage de l'offre gagnant
-	$db->query("update km_offre set off_winner=1 where off_mercato_id={$mercatoId} and off_joueur_id={$joueurId} and off_ekyp_id={$ekypId}");
+	$db->query("update km_offre set off_winner=1 where off_mercato_id={$mercatoId} and off_joueur_id={$joueurId} and off_inscription_id={$inscriptionId}");
 	// Existing engagement ?
-	$existingQ = "select eng_id, eng_ekyp_id from km_engagement where eng_joueur_id={$joueurId} and eng_date_depart IS NULL";
+	$existingQ = "select eng_id, eng_inscription_id from km_engagement inner join km_inscription on eng_inscription_id=ins_id inner join km_championnat on ins_championnat_id=chp_id inner join km_mercato on mer_championnat_id=chp_id where eng_joueur_id={$joueurId} and mer_id={$mercatoId} and eng_date_depart IS NULL";
 	$existing = $db->getArray($existingQ);
 	$joueurQ = "select prenom, nom from joueur where id={$joueurId} limit 1";
 	$joueur = $db->getArray($joueurQ);
@@ -84,7 +84,7 @@ function registerEngagement($ekypId,$joueurId,$montant,$journee,$redistrib, $nbF
 		$exId=$existing[0]['eng_id'];
 		$db->query("update km_engagement set eng_date_depart=now() where eng_id={$exId}");
 		$event = "Vente du joueur {$nomJoueur}";
-		registerFinances($exEkypId,$montant,$event);
+		registerFinances($inscriptionId,$montant,$event);
 	} else {
 		// Redistribution de la somme dépensée
 		$part = (float) ((float)$montant) / ((float) ($nbFranchises-1)) ;
@@ -95,20 +95,20 @@ function registerEngagement($ekypId,$joueurId,$montant,$journee,$redistrib, $nbF
 		}
 	}
 	// Enregistrement de l'engagement avec l'acheteur
-	$db->query("insert into km_engagement(eng_ekyp_id,eng_joueur_id,eng_salaire,eng_date_arrivee,eng_montant) values ({$ekypId},{$joueurId},(select scl_salaire from km_const_salaire_classe inner join km_join_joueur_salaire on scl_id=jjs_salaire_classe_id where jjs_journee_id={$journee} and jjs_joueur_id={$joueurId}), now(), {$montant})");
+	$db->query("insert into km_engagement(eng_inscription_id,eng_joueur_id,eng_salaire,eng_date_arrivee,eng_montant) values ({$inscriptionId},{$joueurId},(select scl_salaire from km_const_salaire_classe inner join km_join_joueur_salaire on scl_id=jjs_salaire_classe_id where jjs_journee_id={$journee} and jjs_joueur_id={$joueurId}), now(), {$montant})");
 	// Finances
 	$event = "Achat du joueur {$nomJoueur}";
-	registerFinances($ekypId,-$montant,$event);
+	registerFinances($inscriptionId,-$montant,$event);
 	return $redistrib;
 }
 
-function registerFinances($ekypId,$montant,$event) {
+function registerFinances($inscriptionId,$montant,$event) {
 	global $db;
 	// Récup du solde actuel de la franchise
-	$soldeQ = "select fin_solde from km_finances where fin_ekyp_id={$ekypId} order by fin_date desc, fin_id desc limit 1";
+	$soldeQ = "select fin_solde from km_finances where fin_inscription_id={$inscriptionId} order by fin_date desc, fin_id desc limit 1";
 	$solde = $db->getArray($soldeQ);
 	$nouveauSolde = round(floatval($solde[0][0]) + ((float) $montant),1); // montant positif ou negatif
-	$db->query("insert into km_finances(fin_ekyp_id,fin_date,fin_transaction,fin_solde,fin_event) values ({$ekypId},now(),{$montant},{$nouveauSolde},'{$event}')");
+	$db->query("insert into km_finances(fin_inscription_id,fin_date,fin_transaction,fin_solde,fin_event) values ({$inscriptionId},now(),{$montant},{$nouveauSolde},'{$event}')");
 }
 
 header('Location: ../index.php');
