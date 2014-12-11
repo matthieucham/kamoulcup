2eace96c6a4da32b95dbbaf6ce58624b0a242878
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2014-12-11 10:10:41 +0100

    Protection contre renvoi-rachat en cours de mercato

--- kamoulcup/km/api/clubs.php
+++ kamoulcup/km/api/clubs.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
 include("../model/model_include.php");
-include("../ctrl/journeeManager.php");
+include("../ctrl/mercatoManager.php");
 
-//$lastJQ = "select id from journee order by date desc limit 1";
-//$lastJ = $db->getArray($lastJQ);
-$lastJ = getLastJournee();
-$lastJourneeId = $lastJ['id'];
 $champId = $_SESSION['myChampionnatId'];
+$mercato = getCurrentMercato($champId);
 
 $clubsQ = "select id, nom from club order by nom asc";
 $clubs = $db->getArray($clubsQ);
@@ -15,7 +12,7 @@ $clubs = $db->getArray($clubsQ);
 $data = array();
 foreach($clubs as $currentClub) {
     $clubId = $currentClub['id'];
-    $joueursQ = "select jo.id, jo.prenom, jo.nom, jo.poste, scl_salaire, max(eng_id) as idEngagement, max(ltr_montant) as montant from joueur jo inner join km_join_joueur_salaire on jo.id=jjs_joueur_id inner join km_const_salaire_classe on scl_id=jjs_salaire_classe_id inner join km_inscription on ins_championnat_id={$champId} left outer join km_engagement on eng_joueur_id=jo.id  and eng_inscription_id=ins_id and  eng_date_depart is null left outer join km_liste_transferts on eng_id=ltr_engagement_id where jo.club_id={$clubId} and jjs_journee_id=(select jjs_journee_id from km_join_joueur_salaire inner join journee on journee.id=jjs_journee_id order by date desc limit 1) group by jo.id order by field(jo.poste,'G','D','M','A'), jo.nom, eng_montant desc ";
+    $joueursQ = "select jo.id, jo.prenom, jo.nom, jo.poste, scl_salaire, max(eng_id) as idEngagement, max(ltr_montant) as montant from joueur jo inner join km_join_joueur_salaire on jo.id=jjs_joueur_id inner join km_const_salaire_classe on scl_id=jjs_salaire_classe_id inner join km_inscription on ins_championnat_id={$champId} inner join km_mercato on mer_id={$mercato['mer_id']} left outer join km_engagement on eng_joueur_id=jo.id  and eng_inscription_id=ins_id and (eng_date_depart is null or eng_date_depart>mer_date_ouverture) left outer join km_liste_transferts on eng_id=ltr_engagement_id where jo.club_id={$clubId} and jjs_journee_id=(select jjs_journee_id from km_join_joueur_salaire inner join journee on journee.id=jjs_journee_id order by date desc limit 1) group by jo.id order by field(jo.poste,'G','D','M','A'), jo.nom, eng_montant desc ";
     $joueursClub = $db->getArray($joueursQ);
     $effectifClub = array();
     if ($joueursClub != NULL) {
