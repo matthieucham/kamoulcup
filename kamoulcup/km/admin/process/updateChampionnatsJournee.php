b60861ffd57ae404cbf0de3838bc0ddf1e510845
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2014-12-16 17:07:12 +0100

    Fix selection avec sub NULL

--- kamoulcup/km/admin/process/updateChampionnatsJournee.php
+++ kamoulcup/km/admin/process/updateChampionnatsJournee.php
@@ -53,8 +53,7 @@ if ($inscriptions != NULL) {
             $db->query($copyQ);
         }
     
-        $possibleCompoQ = "select cro_id,joueur.poste,eng_id,sro_substitute from km_engagement inner join joueur on eng_joueur_id=joueur.id inner join km_championnat_round on cro_journee_id={$journeeId} inner join journee on cro_journee_id=journee.id left outer join km_selection_round on sro_round_id=cro_id and sro_engagement_id=eng_id where (date>=eng_date_arrivee) and (date<eng_date_depart or eng_date_depart IS NULL) and eng_inscription_id={$fr['ins_id']} order by field(poste,'G','D','M','A'),sro_substitute,eng_salaire desc, eng_date_arrivee desc";
-    // ATTENTION BUG A CORRIGER : les substitute "0" sont forcément avant les "NULL" : erreur !
+        $possibleCompoQ = "select cro_id,joueur.poste,eng_id,coalesce(sro_substitute,1) as sub from km_engagement inner join joueur on eng_joueur_id=joueur.id inner join km_championnat_round on cro_journee_id={$journeeId} inner join journee on cro_journee_id=journee.id left outer join km_selection_round on sro_round_id=cro_id and sro_engagement_id=eng_id where (date>=eng_date_arrivee) and (date<eng_date_depart or eng_date_depart IS NULL) and eng_inscription_id={$fr['ins_id']} order by field(poste,'G','D','M','A'),sub,eng_salaire desc, eng_date_arrivee desc";
         $selection=$db->getArray($possibleCompoQ);
         if ($selection != NULL) {
             adjustSelection($selection,$db);
@@ -97,19 +96,23 @@ function adjustSelection($selection,$db) {
     $minPoste=array('G'=>$KM_minG,'D'=>$KM_minD,'M'=>$KM_minM,'A'=>$KM_minA);
     foreach ($selection as $sel) {
         $poste = $sel['poste'];
-        if (($sel['sro_substitute'] == NULL || $sel['sro_substitute']==1) && $countPoste[$poste]<$minPoste[$poste]) {
+        if ($sel['sub']==1 && $countPoste[$poste]<$minPoste[$poste]) {
             // Insérer cette nouvelle ligne de selection
             $insertQ="insert into km_selection_round(sro_engagement_id,sro_round_id,sro_substitute) values ({$sel['eng_id']},{$sel['cro_id']},0) on duplicate key update sro_substitute=0";
             $db->query($insertQ);
             $countPoste[$poste] = $countPoste[$poste]+1;
-        } else if ($sel['sro_substitute']==0 && $countPoste[$poste]>=$minPoste[$poste]) {
+        } else if ($sel['sub']==0 && $countPoste[$poste]>=$minPoste[$poste]) {
             // Enregistrement "en trop", à supprimer !
             $supprQ="insert into km_selection_round(sro_engagement_id,sro_round_id,sro_substitute) values ({$sel['eng_id']},{$sel['cro_id']},1) on duplicate key update sro_substitute=1";
             $db->query($supprQ);
         } else {
-            if ($sel['sro_substitute']==0) {
+            if ($sel['sub']==0) {
                 // C'est un titulaire valide qui fait augmenter le compteur.
                 $countPoste[$poste] = $countPoste[$poste]+1;
+            } else {
+                // Ce joueur doit apparaître en tant que sub dans la sélection
+                $insertQ="insert into km_selection_round(sro_engagement_id,sro_round_id,sro_substitute) values ({$sel['eng_id']},{$sel['cro_id']},1) on duplicate key update sro_substitute=1";
+            $db->query($insertQ);
             }
         }
     }
