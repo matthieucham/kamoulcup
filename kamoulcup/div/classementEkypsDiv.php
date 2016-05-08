df12f93d71812450ddee21ef386b5709ad92f03c
matthieucham <matthieu.cham@gmail.com>
2016-05-08 23:04:13 +0200

    Paramétrisation scores

--- kamoulcup/div/classementEkypsDiv.php
+++ kamoulcup/div/classementEkypsDiv.php
@@ -43,6 +43,6 @@ function generateClassement($nomClassement, $scoreField) {
 }
 
 //generateClassement("Apertura", "score1");
-generateClassement("Saison", "score");
-generateClassement("Clausura", "score2");
+generateClassement("Classement Kamoulcup", "score");
+//generateClassement("Clausura", "score2");
 ?>
--- kamoulcup/process/params/notationParams.php
+++ kamoulcup/process/params/notationParams.php
@@ -1,21 +1,21 @@
 <?php
-	$NOT_leader = Array('G' => 0,'D' => 1.2, 'M' => 0.6, 'A' => 0);
-	$NOT_defvierge = Array('G' => 3.4,'D' => 2.5, 'M' => 1, 'A' => 0);
-	$NOT_defunpenalty = Array('G' => 1.7,'D' => 1.25, 'M' => 0.5, 'A' => 0);
+	$NOT_leader = Array('G' => 0,'D' => 0.3, 'M' => 0.15, 'A' => 0);
+	$NOT_defvierge = Array('G' => 1,'D' => 0.75, 'M' => 0.25, 'A' => 0);
+	$NOT_defunpenalty = Array('G' => 0.5,'D' => 0.375, 'M' => 0.125, 'A' => 0);
 	$NOT_defunbut = Array('G' => 0,'D' => 0, 'M' => 0, 'A' => 0);
-	$NOT_arret = Array('G' => 0.4,'D' => 0, 'M' => 0, 'A' => 0);
-	$NOT_encaisse = Array('G' => -1.2,'D' => 0, 'M' => 0, 'A' => 0);
+	$NOT_arret = Array('G' => 0.075,'D' => 0, 'M' => 0, 'A' => 0);
+	$NOT_encaisse = Array('G' => -0.3,'D' => 0, 'M' => 0, 'A' => 0);
 	
-	$NOT_troisbuts = Array('G' => 0,'D' => 0, 'M' => 0.4, 'A' => 1);
-	$NOT_troisbutsunpenalty = Array('G' => 0,'D' => 0, 'M' => 0.2, 'A' => 0.5);
-	$NOT_but = 3;
-	$NOT_penalty = 1.5;
-	$NOT_passedec = 2;
-	$NOT_penaltyobtenu = 1;
+	$NOT_troisbuts = Array('G' => 0,'D' => 0, 'M' => 0.1, 'A' => 0.25);
+	$NOT_troisbutsunpenalty = Array('G' => 0,'D' => 0, 'M' => 0.05, 'A' => 0.125);
+	$NOT_but = 0.75;
+	$NOT_penalty = 0.375;
+	$NOT_passedec = 0.5;
+	$NOT_penaltyobtenu = 0.25;
 	
 	$BON_ekypcoeff = 0.1;
 	
-	$SCO_nbperfs = 26;
+	$SCO_nbperfs = 3;
 	$SCO_notedefaut = 3;
 	$SCO_minTps =30;
 	$SCO_minTpsCollectif =45;
@@ -23,7 +23,7 @@
 	$SCO_noteEC =1;
 	$SCO_noteEL =3;
 	
-	$SCO_journeePivot=20;
-	$SCO_nbperfs1=13;
-	$SCO_nbperfs2=13;	
+	$SCO_journeePivot=3;
+	$SCO_nbperfs1=3;
+	$SCO_nbperfs2=3;	
 ?>
