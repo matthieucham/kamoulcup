780b784459d2b343a2529be65eccf8b45b64897f
matthieucham <matthieu.cham@gmail.com>
2016-06-25 22:29:08 +0200

    Traitement des "journées" à élimination directe

--- kamoulcup/process/doImport.php
+++ kamoulcup/process/doImport.php
@@ -7,6 +7,15 @@ include('params/statnutsParams.php');
 include('api_import.php');
 include('notation.php');
 
+$elims = $_POST['elim'];
+$nb = count($elims);
+for ($i=0;$i<$nb;$i++) {
+	$uuid = $elims[$i];
+	// update eliminatoire flag of current journee
+	$q = "update journee set eliminatoire=1 where uuid='{$uuid}' limit 1";
+	$db->query($q);
+}
+
 $uuids = $_POST['syncme'];
 $nb = count($uuids);
 $token = getAccessToken();
