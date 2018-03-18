bf670cfc8ac0fb6905ba8554bbfff824ef0dd364
matthieucham <matthieu.cham@gmail.com>
2018-03-18 15:13:42 +0100

    migrate statnuts
    # Conflicts:
    #	kamoulcup/process/params/statnutsParams.php

--- kamoulcup/process/params/statnutsParams.php
+++ kamoulcup/process/params/statnutsParams.php
@@ -1,6 +1,6 @@
 <?php
-$SN_instance = '2e336129-3be7-4b93-af88-aa53891dba51';
-$SN_host = 'statnuts-kcup.rhcloud.com';
+$SN_instance = 'ef51754d-1a4d-4116-8d64-1de77ca23fcb';
+$SN_host = 'statnuts-app-statnuts.193b.starter-ca-central-1.openshiftapps.com';
 $SN_client_id = 'ddjfezzejfggelzlkehsgazehd';
 $SN_client_secret = 'hazhgygdcsksdjhqmFEFREzadkezaappppzeRz';
 
@@ -10,6 +10,7 @@ $SN_src_WS = '04c19d53-ba15-11e4-97c6-b1229586dec7';
 $SN_src_SP = '0ecffaee-ba15-11e4-97c6-b1229586dec7';
 $SN_src_EQ = '193bc965-ba15-11e4-97c6-b1229586dec7';
 $SN_src_KI = '099e0a06-bba1-11e4-aabd-e33b7dc35c80';
+$SN_src_OS = 'f2750ce3-bef0-46a2-89aa-83f4042eb931';
 
 $SN_test_step = 'ae01dbd1-f83f-4b95-90e7-bef46aa6b2c7';
 
