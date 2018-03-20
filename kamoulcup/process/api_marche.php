5f6206fa4b1960249b4f5004ea49ef018e60c5f0
matthieucham <matthieu.cham@gmail.com>
2018-03-20 22:40:20 +0100

    Fix bug a la fin de période

--- kamoulcup/process/api_marche.php
+++ kamoulcup/process/api_marche.php
@@ -7,9 +7,9 @@
 function getOpenPeriode($poule,$withDraft) {
 	global $db;
 	if ($withDraft) {
-		$periodeEnCours = $db->getArray("select date_format(date_debut,'%d/%m %H:%i') as debut, date_format(date_fin,'%d/%m %H:%i') as fin, delai_encheres,draft from periode where date_debut < now() and date_fin > now() and poule_id={$poule}");
+		$periodeEnCours = $db->getArray("select date_format(date_debut,'%d/%m %H:%i') as debut, date_format(date_fin,'%d/%m %H:%i') as fin, delai_encheres,draft,(date_debut<now() and date_fin>now()) as paPossible from periode where date_debut < now() and date_add(date_fin, interval delai_encheres hour) > now() and poule_id={$poule}");
 	} else {
-		$periodeEnCours = $db->getArray("select date_format(date_debut,'%d/%m %H:%i') as debut, date_format(date_fin,'%d/%m %H:%i') as fin, delai_encheres,draft from periode where date_debut < now() and date_fin > now() and poule_id={$poule} and draft=0");
+		$periodeEnCours = $db->getArray("select date_format(date_debut,'%d/%m %H:%i') as debut, date_format(date_fin,'%d/%m %H:%i') as fin, delai_encheres,draft,(date_debut<now() and date_fin>now()) as paPossible from periode where date_debut < now() and date_add(date_fin, interval delai_encheres hour) > now() and poule_id={$poule} and draft=0");
 	}
 	return $periodeEnCours;
 }
